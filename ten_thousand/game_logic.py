f3a9a464af53ea2e7b22f7b4c9bde21f12b79b07
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-05-15 09:10:17 +0000

    roll dic 6 tests passed

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -4,8 +4,9 @@ class GameLogic:
   
     @staticmethod
     def roll_dice(n):
-        pass
-    
+        """Return a tuple of n random dice rolls."""
+        return tuple(random.randint(1, 6) for dice in range(n))
+
     @staticmethod
     def calculate_score(dice):
         pass
