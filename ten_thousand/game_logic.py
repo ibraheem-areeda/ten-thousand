020249c546976fa7043f16b9be6adb33bfef3087
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-05-15 09:13:19 +0000

    calculate_score 7 tests failed, 8 tests passed

--- ten_thousand/game_logic.py
+++ ten_thousand/game_logic.py
@@ -9,4 +9,39 @@ class GameLogic:
 
     @staticmethod
     def calculate_score(dice):
-        pass
+        """Calculate the score for a roll of dice."""
+        if not dice: # if dice == None return 0, 
+            return 0
+
+        counts = []
+        for value in range(1, 7):
+          counts.append(dice.count(value))
+        score = 0
+
+        # Handle 1's and 5's
+        score += counts[0] * 100
+        score += counts[4] * 50
+
+        # Handle 3 or more of a kind
+        for i in range(1, 7):
+            if counts[i-1] >= 3:
+                if i == 1:
+                    score += 1000 * (counts[i-1] // 3)
+                else:
+                    score += 100 * i * (counts[i-1] // 3)
+                counts[i-1] %= 3
+
+        # Handle straight
+        if all(count == 1 for count in counts):
+            score = 1500
+
+        # Handle 3 pairs
+        if len([count for count in counts if count == 2]) == 3:
+            score = 1500
+
+        # Handle 2 triplets
+        if len([count for count in counts if count == 3]) == 2:
+            score = 2500
+
+        return score
+        
